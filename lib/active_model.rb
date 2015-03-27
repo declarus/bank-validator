32625a4770320adac0f3db0c199524ce158b9fd3
Adam Bahlke <adam.bahlke@hitfoxgroup.com>
2015-03-27 09:52:37 +0100

    initial commit

---
+++ lib/active_model.rb
@@ -0,0 +1,5 @@
+require 'extensions/active_model/validations/iban_validator'
+
+module ActiveModel
+  autoload :IbanValidator, 'active_model/validations/iban_validator'
+end
---
+++ lib/bank-validator.rb
@@ -0,0 +1,1 @@
+require 'bank-validator/active_model/active_model'
